c449dd50bca50d68aa94877d1c75c0c15e30a4f8
alvarofloresf <45455677+alvarofloresf@users.noreply.github.com>
2022-04-08 20:10:38 -0400

    P2-001 Created users controller and .gitignore

---
+++ Practica 2 - Tec Web/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica_2___Tec_Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult PostUser()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteUser()
+
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult PutUser()
+        {
+            return Ok();
+        }
+    }
+}
